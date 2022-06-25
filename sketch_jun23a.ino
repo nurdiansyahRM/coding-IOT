3d2e4bc3dc96157306dd2acdfb32c99e97a5e2b7
Nurdiansyah RM <70813174+nurdiansyahRM@users.noreply.github.com>
2022-06-25 11:21:46 +0700

    Add files via upload

---
+++ sketch_jun23a.ino
@@ -0,0 +1,49 @@
+#include "CTBot.h"
+CTBot robot;
+int relay = 4;
+
+String ssid_wifi = "OPPO A53";
+String password = "nurdin008";
+String api_token = "5573467370:AAGkWbs-cVwAuhKgbOJ8PofcoCAeeFW-lUY";
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, HIGH);
+
+  // Konek ke wifi
+  robot.wifiConnect(ssid_wifi, password);
+
+  // Konek ke telegram
+  robot.setTelegramToken(api_token);
+  
+  // Cek koneksi
+  if(robot.testConnection())
+    Serial.println("Terhubung :)");
+  else
+    Serial.println("Error!");
+}
+
+void loop() {
+
+  TBMessage message;
+
+  if(robot.getNewMessage(message)) {
+  
+    Serial.print("Ada pesan Masuk: ");
+    Serial.println(message.text);     
+    
+    if(message.text.equalsIgnoreCase("ON")) {
+      digitalWrite(relay, LOW);
+      robot.sendMessage(message.sender.id, "Lampu Menyala");
+    } else if(message.text.equalsIgnoreCase("OFF")) {
+      digitalWrite(relay, HIGH);
+      robot.sendMessage(message.sender.id, "Lampu Mati");   
+    } else {
+      String reply;
+      reply = ("Perintah salah. Coba kirim ON atau OFF!");
+      robot.sendMessage(message.sender.id, reply);
+    }
+    
+  }
+}
